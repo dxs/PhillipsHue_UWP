60bba72f3e5b24762ed659019b26dd7ed1a78fe7
Sven <glaglasven@live.com>
2016-09-14 21:19:58 +0200

    #lib add Groups #1 (not finished)

---
+++ PhillipsHue_UWP/HueLib/Groups.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhillipsHue_UWP.HueLib
+{
+	/// <summary>
+	/// Represents a Hue Group
+	/// </summary>
+	/// <remarks>http://www.developers.meethue.com/documentation/groups-api</remarks>
+	[DataContract]
+	class Groups
+	{
+		/// <summary>
+		/// An internal backlink to the group's bridge
+		/// </summary>
+		internal Bridge _bridge;
+
+		/// <summary>
+		/// Gets or sets the group's id
+		/// </summary>
+		[DataMember(Name = "id")]
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the group's name
+		/// </summary>
+		[DataMember(Name = "name")]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the group's lights
+		/// </summary>
+		[DataMember(Name = "lights")]
+		public string[] Lights { get; set; }
+
+		/// <summary>
+		/// Gets or sets the group's type
+		/// </summary>
+		[DataMember(Name = "type")]
+		public string Type { get; set; }
+
+		/// <summary>
+		/// Gets or sets the group's action
+		/// </summary>
+		[DataMember(Name = "action")]
+		public Action Action { get; set; }
+	}
+
+	[DataContract]
+	public class Action
+	{
+		/// <summary>
+		/// An internal backlink to this state's group
+		/// </summary>
+		internal Groups _groups;
+
+		//[DataMember(Name = "on")]
+		/*Todo*/
+	}
+}
---
+++ PhillipsHue_UWP/HueLib/Schedules.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhillipsHue_UWP.HueLib
+{
+	/// <summary>
+	/// http://www.developers.meethue.com/documentation/schedules-api-0
+	/// </summary>
+	class Schedules
+	{
+	}
+}
