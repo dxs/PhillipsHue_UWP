53c868829537c7914dbfc95390c8c9172e5d70be
Sven <glaglasven@live.com>
2016-09-14 12:24:24 +0200

    Lib #1 End

---
+++ PhillipsHue/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using PhillipsHue_UWP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PhillipsHue
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+			Setup();
+        }
+
+		private async void Setup()
+		{
+			Bridge bridge = await Bridge.FindAsync();
+			await bridge.RegisterAsync();
+			IEnumerable<Light> lights = await bridge.GetLightsAsync();
+			foreach (Light light in lights)
+			{
+				light.State.On = true;
+			}
+		}
+	}
+}
